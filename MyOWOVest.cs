51d6945e0e5a96f37ac06634b1e0aed77d62583e
floh-bhaptics <floh@fahrenbergers.de>
2022-11-03 06:57:53 +0100

    Add project files.

---
+++ MyOWOVest.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading;
+using MelonLoader;
+using OWOHaptic;
+//using MyOWOSensations;
+
+namespace MyOWOVest
+{
+    public class TactsuitVR
+    {
+        /* A class that contains the basic functions for the bhaptics Tactsuit, like:
+         * - A Heartbeat function that can be turned on/off
+         * - A function to read in and register all .tact patterns in the bHaptics subfolder
+         * - A logging hook to output to the Melonloader log
+         * - 
+         * */
+        public bool suitDisabled = true;
+        public bool systemInitialized = false;
+        // Event to start and stop the heartbeat thread
+        public Dictionary<String, ISensation> FeedbackMap = new Dictionary<String, ISensation>();
+
+
+        /*
+        //public static ISensation Explosion => new Sensation(100, 1f, 80, 100f, 500f, 0f);
+        public static Sensation Explosion = Sensation.Create(100, 1f, 80, 100f, 500f, 0f);
+        public static ISensation ExplosionBelly = Sensation.CreateWithMuscles(Explosion, Muscle.Lumbar_L, Muscle.Lumbar_R, Muscle.Abdominal_L, Muscle.Abdominal_R);
+        //public static OWOSensationWithMuscles ExplosionBelly = new OWOSensationWithMuscles(Explosion, OWOMuscle.Abdominal_Left, OWOMuscle.Abdominal_Right, OWOMuscle.Lumbar_Left, OWOMuscle.Lumbar_Right);
+
+        public static Sensation Healing = Sensation.Create(70, 0.5f, 65, 300f, 200f, 0f);
+        public static ISensation HealingBody = Sensation.CreateWithMuscles(Healing, Muscle.AllMuscles);
+
+        
+        public static Sensation Reload1 = Sensation.Create(100, 0.3f, 50, 100f, 100f, 0f);
+        public static Sensation Reload2 = Sensation.Create(100, 0.2f, 40, 0f, 100f, 0f);
+        public static ISensation Reloading = Reload1.ContinueWith(Reload2);
+        */
+
+        public TactsuitVR()
+        {
+            LOG("Initializing suit");
+            //OWO.AutoConnect();
+            OWO.Connect("192.168.1.183");
+            Thread.Sleep(100);
+            RegisterAllTactFiles();
+            if (OWO.IsConnected)
+            {
+                suitDisabled = false;
+                LOG("OWO suit connected.");
+            }
+            if (suitDisabled) LOG("Owo is not enabled?!?!"); 
+
+            LOG("Starting up...");
+            PlayBackFeedback("Start");
+        }
+
+        ~TactsuitVR()
+        {
+            LOG("Destructor called");
+            DisconnectOwo();
+        }
+
+        public void DisconnectOwo()
+        {
+            LOG("Disconnecting Owo skin.");
+            OWO.Disconnect();
+        }
+
+        public void LOG(string logStr)
+        {
+#pragma warning disable CS0618 // remove warning that the logger is deprecated
+            MelonLogger.Msg(logStr);
+#pragma warning restore CS0618
+        }
+
+        void RegisterAllTactFiles()
+        {
+            
+            string configPath = Directory.GetCurrentDirectory() + "\\Mods\\OWO";
+            DirectoryInfo d = new DirectoryInfo(configPath);
+            FileInfo[] Files = d.GetFiles("*.owo", SearchOption.AllDirectories);
+            for (int i = 0; i < Files.Length; i++)
+            {
+                string filename = Files[i].Name;
+                string fullName = Files[i].FullName;
+                string prefix = Path.GetFileNameWithoutExtension(filename);
+                // LOG("Trying to register: " + prefix + " " + fullName);
+                if (filename == "." || filename == "..")
+                    continue;
+                string tactFileStr = File.ReadAllText(fullName);
+                try
+                {
+                    ISensation test = Sensation.FromCode(tactFileStr);
+                    //bHaptics.RegisterFeedback(prefix, tactFileStr);
+                    LOG("Pattern registered: " + prefix);
+                    FeedbackMap.Add(prefix, test);
+                }
+                catch (Exception e) { LOG(e.ToString()); }
+
+            }
+            
+            systemInitialized = true;
+        }
+
+
+        public void PlayBackHit()
+        {
+            Sensation sensation = Sensation.ShotEntry;
+            // two parameters can be given to the pattern to move it on the vest:
+            // 1. An angle in degrees [0, 360] to turn the pattern to the left
+            // 2. A shift [-0.5, 0.5] in y-direction (up and down) to move it up or down
+            OWO.Send(sensation, Muscle.Pectoral_L, Muscle.Pectoral_R);
+        }
+
+        public void Recoil(bool isRightHand, bool isTwoHanded = false)
+        {
+            if (isTwoHanded)
+            {
+                PlayBackFeedback("Recoil_L");
+                PlayBackFeedback("Recoil_R");
+                //OWO.Send(Sensation.GunRecoil, Muscle.Arm_R.WithIntensity(70), Muscle.Arm_L.WithIntensity(70));
+                return;
+            }
+            if (isRightHand) PlayBackFeedback("Recoil_R");
+            else PlayBackFeedback("Recoil_L");
+        }
+
+        public void PlayBackFeedback(string feedback, float intensity = 1.0f)
+        {
+            if (FeedbackMap.ContainsKey(feedback))
+            {
+                OWO.Send(FeedbackMap[feedback]);
+            }
+            else LOG("Feedback not registered: " + feedback);
+        }
+
+    }
+}
---
+++ UntilYouFall_OWO.cs
@@ -0,0 +1,346 @@
+﻿using System;
+using MelonLoader;
+using HarmonyLib;
+using System.Threading;
+
+using SG.Claymore.Movement.Dash;
+using SG.Claymore.Combat.Blocking;
+using SG.Claymore.Interaction;
+using SG.Claymore.Armaments;
+using SG.Claymore.Armaments.Abilities;
+using SG.Claymore.Player;
+using SG.Claymore.Combat.EnemyAttacks;
+using SG.Claymore.Entities;
+using SG.Claymore.Armory;
+using MyOWOVest;
+
+namespace UntilYouFall_OWO
+{
+    public class UntilYouFall_OWO : MelonMod
+    {
+        public static TactsuitVR tactsuitVr;
+        private static String ActiveHand = "PlayerHandRight";
+        private static ManualResetEvent mrse = new ManualResetEvent(false);
+        private static bool BulwarkActive = false;
+        private static bool handsConnected = true;
+
+        private static void HeartBeatFunc()
+        {
+            while (true)
+            {
+                mrse.WaitOne();
+                tactsuitVr.PlayBackFeedback("HeartBeat");
+                Thread.Sleep(1000);
+            }
+        }
+
+        public override void OnUpdate()
+        {
+        }
+
+
+        public override void OnInitializeMelon()
+        {
+            tactsuitVr = new TactsuitVR();
+        }
+
+
+        [HarmonyPatch(typeof(PlayerDash), "OnDashForward")]
+        public class DashForward
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerDash __instance)
+            {
+                //tactsuitVr.LOG("OnDashForward.");
+                try
+                {
+                    //TactsuitVR.FeedbackType feedback = TactsuitVR.FeedbackType.DashForward;
+                    //TactsuitUntilYouFall.tactsuitVr.SimpleFeedback(feedback);
+                    tactsuitVr.PlayBackFeedback("DashForward");
+                }
+                catch(Exception e) { tactsuitVr.LOG("Feedback failed. " + e.ToString()); }
+            }
+        }
+
+        [HarmonyPatch(typeof(Armament), "InitSummon")]
+        public class InitSummon
+        {
+            [HarmonyPostfix]
+            public static void Postfix(Armament __instance, PlayerHand summoningHand)
+            {
+                if (summoningHand.name == "PlayerHandLeft")
+                {
+                    // tactsuitVr.LOG("MeleeWeapon.Summon Left");
+                    tactsuitVr.PlayBackFeedback("SummonWeapon_L");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("SummonWeaponHands_L"); }
+                }
+                else
+                {
+                    // tactsuitVr.LOG("MeleeWeapon.Summon Right");
+                    tactsuitVr.PlayBackFeedback("SummonWeapon_R");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("SummonWeaponHands_R"); }
+                }
+            }
+        }
+
+
+        [HarmonyPatch(typeof(MeleeWeapon), "GetForceRating", new Type[] { typeof(SG.Claymore.HitSystem.HitData.HitQualityType) })]
+        public class HitForce
+        {
+            [HarmonyPostfix]
+            public static void Postfix(MeleeWeapon __instance, float __result)
+            {
+
+                if (__result > 1.0)
+                {
+                    float intensity = __result / 10.0f + 0.02f;
+                    if (__instance.armament.BoundHand == __instance.HoldingPlayer.LeftHand)
+                    {
+                        tactsuitVr.PlayBackFeedback("Block_L", intensity);
+                        if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_L", intensity); }
+                        if (__instance.armament.isHeldInTwoHands)
+                        {
+                            tactsuitVr.PlayBackFeedback("Block_R", intensity * 0.5f);
+                            if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_R", intensity * 0.5f); }
+                        }
+                    }
+                        else
+                    {
+                        tactsuitVr.PlayBackFeedback("Block_R", intensity);
+                        if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_R", intensity); }
+                        if (__instance.armament.isHeldInTwoHands)
+                        {
+                            tactsuitVr.PlayBackFeedback("Block_L", intensity * 0.5f);
+                            if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_L", intensity * 0.5f); }
+                        }
+                    }
+                    // tactsuitVr.LOG("GetForceRating");
+                    // tactsuitVr.LOG(__result.ToString());
+
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(ArmamentAbilityUser), "OnSuperActivated")]
+        public class SuperActivated
+        {
+            [HarmonyPostfix]
+            public static void Postfix(ArmamentAbilityUser __instance)
+            {
+                // tactsuitVr.LOG("ArmamentAbilityUser");
+                if (__instance.HoldingPlayer.LeftHand == __instance.armament.BoundHand)
+                {
+                    tactsuitVr.PlayBackFeedback("CrushCrystal_L");
+                    tactsuitVr.PlayBackFeedback("ActivateSuper_L");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("CrushCrystalHands_L"); }
+                } else
+                {
+                    tactsuitVr.PlayBackFeedback("CrushCrystal_R");
+                    tactsuitVr.PlayBackFeedback("ActivateSuper_R");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("CrushCrystalHands_R"); }
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerHealth), "OnHealthChanged")]
+        public class HealthChanged
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerHealth __instance, float hp)
+            {
+                BulwarkActive = false;
+                // tactsuitVr.LOG(hp.ToString());
+                if (hp >= 1.0f)
+                {
+                    mrse.Reset();
+                }
+                if(__instance.IsDead)
+                {
+                    mrse.Reset();
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerDefense), "OnAttackHit")]
+        public class OnAttackHit
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerDefense __instance, BlockTimingData blockTiming)
+            {
+                // tactsuitVr.LOG("PlayerDefense.OnAttackHit");
+                if (__instance.health.IsOnDeathsDoor)
+                {
+                    mrse.Set();
+                }
+                if (__instance.health.IsDead)
+                {
+                    mrse.Reset();
+                }
+                if (BulwarkActive)
+                {
+                    // tactsuitVr.LOG("Bulwark active");
+                    tactsuitVr.PlayBackFeedback("Bulwark");
+                }
+                else if (blockTiming.IsDodgePremonition)
+                {
+                    tactsuitVr.PlayBackFeedback("HitByHammer");
+                }
+                else
+                {
+                    tactsuitVr.PlayBackFeedback("SlashDefault");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerDefense), "OnAttackBlocked")]
+        public class OnAttackBlocked
+        {
+            [HarmonyPostfix]
+            public static void Postfix(PlayerDefense __instance, AttackBlocker blocker)
+            {
+                // tactsuitVr.LOG("PlayerDefense.OnAttackBlocked");
+                if (blocker == blocker.HoldingPlayer.leftBlocker)
+                {
+                    tactsuitVr.PlayBackFeedback("Block_L");
+                    tactsuitVr.PlayBackFeedback("BlockVest_L");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_L"); }
+                    if (blocker.armament.isHeldInTwoHands)
+                    {
+                        tactsuitVr.PlayBackFeedback("Block_R", 0.5f);
+                        tactsuitVr.PlayBackFeedback("BlockVest_R", 0.5f);
+                        if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_R", 0.5f); }
+                    }
+                }
+                else
+                {
+                    tactsuitVr.PlayBackFeedback("Block_R");
+                    tactsuitVr.PlayBackFeedback("BlockVest_R");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_R"); }
+                    if (blocker.armament.isHeldInTwoHands)
+                    {
+                        tactsuitVr.PlayBackFeedback("Block_L", 0.5f);
+                        tactsuitVr.PlayBackFeedback("BlockVest_L", 0.5f);
+                        if (handsConnected) { tactsuitVr.PlayBackFeedback("BlockHands_L", 0.5f); }
+                    }
+                }
+
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerDefense), "KnockbackPlayer")]
+        public class Knockback
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                // tactsuitVr.LOG("PlayerDefense.KnockBackPlayer");
+                tactsuitVr.PlayBackFeedback("KnockBack");
+            }
+        }
+
+        [HarmonyPatch(typeof(CrushInteractable), "OnCrushStart")]
+        public class CrushStart
+        {
+            [HarmonyPostfix]
+            public static void Postfix(CrushInteractable __instance)
+            {
+                if (__instance.crushingHand.name == "PlayerHandLeft")
+                {
+                    //tactsuitVr.LOG("CrushInteractable.OnCrushStart Left");
+                    tactsuitVr.PlayBackFeedback("CrushCrystal_L");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("CrushCrystalHands_L"); }
+                    ActiveHand = "PlayerHandLeft";
+                } else
+                {
+                    //tactsuitVr.LOG("CrushInteractable.OnCrushStart Right");
+                    tactsuitVr.PlayBackFeedback("CrushCrystal_R");
+                    if (handsConnected) { tactsuitVr.PlayBackFeedback("CrushCrystalHands_R"); }
+                    ActiveHand = "PlayerHandRight";
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(CrushInteractable), "OnCancelCrush")]
+        public class CrushCancel
+        {
+            [HarmonyPostfix]
+            public static void Postfix(CrushInteractable __instance)
+            {
+                //tactsuitVr.LOG("CrushInteractable.OnCancelCrush");
+                if (__instance.crushingHand.name == "PlayerHandLeft")
+                {
+                    //tactsuitVr.StopHapticFeedback(TactsuitVR.FeedbackType.CrushCrystal_L);
+                    //tactsuitVr.StopHapticFeedback(TactsuitVR.FeedbackType.CrushCrystalHands_L);
+                }
+                else
+                {
+                    //tactsuitVr.StopHapticFeedback(TactsuitVR.FeedbackType.CrushCrystal_R);
+                    //tactsuitVr.StopHapticFeedback(TactsuitVR.FeedbackType.CrushCrystalHands_R);
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(CrushInteractable), "FinishCrush")]
+        public class CrushComplete
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                if (ActiveHand == "PlayerHandLeft")
+                {
+                    //tactsuitVr.LOG("CrushInteractable.OnCrushStart Left");
+                    tactsuitVr.PlayBackFeedback("CrystalCrushed_L");
+                }
+                else
+                {
+                    //tactsuitVr.LOG("CrushInteractable.OnCrushStart Right");
+                    tactsuitVr.PlayBackFeedback("CrystalCrushed_R");
+                }
+            }
+        }
+
+        [HarmonyPatch(typeof(GroundAttackPlayable), "Raise")]
+        public class GroundAttack
+        {
+            [HarmonyPostfix]
+            public static void Postfix(GroundAttackPlayable __instance)
+            {
+                tactsuitVr.LOG("GroundAttackPlayable.Raise");
+                tactsuitVr.PlayBackFeedback("GroundAttack");
+            }
+        }
+
+        [HarmonyPatch(typeof(PlayerHealth), "Restore")]
+        public class RestoreHealth
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                // tactsuitVr.LOG("PlayerHealth.Restore");
+                tactsuitVr.PlayBackFeedback("Heal");
+            }
+        }
+
+        [HarmonyPatch(typeof(WeaponRackSlot), "OnEnable")]
+        public class WeaponRack
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                tactsuitVr.PlayBackFeedback("RiseAgain");
+            }
+        }
+
+        [HarmonyPatch(typeof(BulwarkAbility), "OnActivationSuccess")]
+        public class ActivateBulwark
+        {
+            [HarmonyPostfix]
+            public static void Postfix()
+            {
+                // tactsuitVr.LOG("Activate Bulwark");
+                BulwarkActive = true;
+            }
+        }
+
+    }
+}
